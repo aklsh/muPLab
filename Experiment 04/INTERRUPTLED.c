f3fa09c714351c00a38a986c536bdaaa09d6c364
Akilesh Kannan <akileshkannan@gmail.com>
2019-09-03 20:55:58 +0530

    dfda

--- Experiment 04/INTERRUPTLED.c
+++ Experiment 04/INTERRUPTLED.c
@@ -9,24 +9,29 @@ ISR (INT1_vect)
 	// with ON and OFF interval of 1 second each
 	for(int i=0; i<10; i=i+1)
 	{
+		//PB0 is set to 1 for 1 sec.
 		PORTB = 0x01;
 		_delay_ms(1000);
+		//PB0 is set to 0 for 1 sec.
 		PORTB = 0x00;
 		_delay_ms(1000);
 	}
 }
-int main(void)
+
+int main (void)
 {
-	DDRD = 0x00 ;
+	//port i/o declarations
+	DDRD = 0x00;
 	DDRB = 0x01;
 	MCUCR = 0x02;
 	GICR = 0x80;
-	PORTB = 0x00 ;
-	
+	PORTB = 0x00;
+
+	//set interrupt flag of SREG
 	sei();
 
 	while (1)
 	{
-		
+		//To keep the program running forever.
 	}
 }
